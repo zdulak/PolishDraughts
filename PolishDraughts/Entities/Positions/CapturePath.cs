45ae28d716ec3672a585be6843a122ef343abb45
zdulak <60813197+zdulak@users.noreply.github.com>
2022-01-13 19:18:33 +0100

    Revert "projects folders renamed"
    This reverts commit d4a583a94b0aec461d8262b7ee48685bc5f0fec3.

---
+++ PolishDraughts/Entities/Positions/CapturePath.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace PolishDraughts.Core.Entities.Positions
+{
+    public record CapturePath(List<Position> Path, List<Position> Captured)
+    {
+        //public override string ToString()
+        //{
+        //    var path = string.Join("->", Path);
+        //    var captured = string.Join(", ", Captured);
+        //    return $"Path:{path}.  Captured pieces: {captured}.";
+        //}
+    }
+}
